21ee9ddd7dc3a533dc5aa6db3379ed33c14ff0c6
Yonatan Karby <jokerkarby@gmail.com>
2022-01-20 12:29:32 +0200

    added LargeMessageSender implemetation

--- MamaStalker/MamaStalker.Core/MessageProtocol/LargeMessageSender.cs
+++ MamaStalker/MamaStalker.Core/MessageProtocol/LargeMessageSender.cs
@@ -1,7 +1,6 @@
 ﻿using MamaStalker.Common.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace MamaStalker.Common.MessageProtocol
 {
@@ -9,11 +8,30 @@ namespace MamaStalker.Common.MessageProtocol
     {
         private ITcpConnection _connection;
         private const int BATCH_SIZE= 1024;
+        private PacketMaker _packetmaker = new PacketMaker();
         public void SendMessage(byte[] data, object headerDto)
         {
             var batches = data.Length / BATCH_SIZE;
-        }
+            var slicedBatches = BatchSlice(data, batches);
+            var packets = new List<PacketInfo>();
+            packets.Add(_packetmaker.CreatePacket(PacketType.Header, headerDto));
+            foreach(var batch in slicedBatches)
+            {
+                packets.Add(_packetmaker.CreatePacket(PacketType.data, batch));
+            }
 
+            Send(packets.ToArray());
+        }
+        private void Send(PacketInfo[] packets)
+        {
+            foreach(var packet in packets)
+            {
+                if(_connection.IsRunning())
+                {
+                    _connection.Write(packet.data);
+                }
+            }
+        }
         private byte[][] BatchSlice(byte[] data, int batches)
         {
             throw new NotImplementedException();
