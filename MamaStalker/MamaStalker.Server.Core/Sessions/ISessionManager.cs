d4239c101dceaea886e9f60fd3fd5cf23fff22d7
Yonatan Karby <jokerkarby@gmail.com>
2022-01-20 10:43:17 +0200

    added tcp session manager class

--- MamaStalker/MamaStalker.Server.Core/Sessions/ISessionManager.cs
+++ MamaStalker/MamaStalker.Server.Core/Sessions/ISessionManager.cs
@@ -3,5 +3,6 @@
     public interface ISessionManager
     {
         void Start(int portNumber);
+        void SendAll(byte[] buffer);
     }
 }
---
+++ MamaStalker/MamaStalker.Server.Core/Sessions/TcpSessionManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaStalker.Server.Core.Sessions
+{
+    public class TcpSessionManager : ISessionManager
+    {
+        public TcpSessionManager()
+        {
+
+        }
+        
+        public void SendAll(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int portNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
