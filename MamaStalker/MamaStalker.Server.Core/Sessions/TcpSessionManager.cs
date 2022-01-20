fa1e3b2be5256a7a39ea7d5a3b420c624fc15598
Yonatan Karby <jokerkarby@gmail.com>
2022-01-20 10:47:03 +0200

    added properties to tcp session manager

--- MamaStalker/MamaStalker.Server.Core/Sessions/TcpSessionManager.cs
+++ MamaStalker/MamaStalker.Server.Core/Sessions/TcpSessionManager.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using MamaStalker.Common.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace MamaStalker.Server.Core.Sessions
 {
     public class TcpSessionManager : ISessionManager
     {
-        public TcpSessionManager()
+        private IEnumerable<ITcpConnection> _connections;
+        private readonly ITcpSessionFactory _sessionFactory;
+        private TcpListener _listner;
+        public TcpSessionManager(ITcpSessionFactory sessionFactory)
         {
-
+            _sessionFactory = sessionFactory;
+            _connections = new List<ITcpConnection>();
         }
         
         public void SendAll(byte[] buffer)
@@ -18,7 +24,8 @@ namespace MamaStalker.Server.Core.Sessions
 
         public void Start(int portNumber)
         {
-            throw new NotImplementedException();
+            _listner = new TcpListener(portNumber);
+            
         }
     }
 }
